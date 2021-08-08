f948ed2958dab5b5a6144bbfc4690f7b14326224
Auyy <71812937+Auyy@users.noreply.github.com>
2021-08-08 20:32:19 +0700

    sqlite mac

---
+++ Yummy/Yummy/Data/YummyContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Yummy.Models;
+
+namespace Yummy.Data
+{
+    public class YummyContext : DbContext
+    {
+        public YummyContext(DbContextOptions<YummyContext> options)
+             : base(options)
+        {
+        }
+
+        public DbSet<TblProduct> TblProduct { get; set; }
+    }
+}
